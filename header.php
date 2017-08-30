df44db14af24678748a6474c7c45709b73a12901
John <hej@johnsvensson.com>
2017-08-30 09:37:27 +0200

    favicon

--- header.php
+++ header.php
@@ -5,6 +5,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+<!-- favicon -->
+<link rel="icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/assets/images/favicon.ico" />
+<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/assets/images/favicon.ico" />
+
 <?php wp_head(); ?>
 </head>
 
