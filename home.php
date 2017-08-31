3831e741840a9d3bc84fa8d7fe9d3d08463db1b0
John <hej@johnsvensson.com>
2017-08-31 07:54:07 +0200

    blog template

---
+++ home.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template Name: Blog
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+<div class="wrap__inner">
+
+  <?php the_post(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+    <article>
+      <h1><?php the_title(); ?></h1>
+      <?php if ( has_excerpt() ) : ?>
+        <?php the_excerpt(); ?>
+      <?php endif; ?>
+      
+      <?php the_content(); ?>
+    </article>
+
+    <?php $a = array( 'post_type' => 'post' ); ?>
+    <?php $q = new WP_Query( $a ); ?>
+    
+    <?php if ( $q->have_posts() ) : ?>
+    
+    <div class="posts">
+      <?php while ( $q->have_posts() ) : $q->the_post(); ?>
+        <article>
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </article>
+      <?php endwhile; ?>
+
+    </div><!-- /posts -->
+    <?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	
+</div><!-- /wrap__inner -->	
+</div><!-- .wrap -->
+
+<?php get_footer();
---
+++ single.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+
+
+<div class="wrap">
+<div class="wrap__inner">
+
+  <?php the_post(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+      <small>Single</small>
+      <h1><?php the_title(); ?></h1>
+      <?php if ( has_excerpt() ) : ?>
+        <?php the_excerpt(); ?>
+      <?php endif; ?>
+      
+      <?php the_content(); ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php /*get_sidebar(); */ ?>
+	
+</div><!-- /wrap__inner -->	
+</div><!-- .wrap -->
+
+<?php get_footer();
